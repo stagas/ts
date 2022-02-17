3804e349bd238dd60f05ddf9a74900b28948d0bb
stagas <gstagas@gmail.com>
2022-02-17 21:06:13 +0200

    feature: disable some more eslint unnecessary rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   ignorePatterns: ['dist', 'node_modules'],
   plugins: ['import'],
   rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
@@ -21,8 +22,10 @@ module.exports = {
     '@typescript-eslint/no-inferrable-types': 'warn',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-var-requires': 'off',
     'no-cond-assign': 'off',
+    'no-empty': 'off',
   },
 }
