5aa795908d9fbe34cf2fe74c64d4d2249bb172e3
stagas <gstagas@gmail.com>
2022-03-23 08:09:01 +0200

    feature: make main a named module file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-/**
- * Adds two numbers together.
- *
- * ```ts
- * add(1, 2) // => 3
- * ```
- *
- * @param a First number
- * @param b Second number
- * @returns The sum of the two numbers
- */
-export const add = (a: number, b: number) => a + b
+export * from './mod'
---
+++ src/mod.ts
@@ -0,0 +1,12 @@
+/**
+ * Adds two numbers together.
+ *
+ * ```ts
+ * add(1, 2) // => 3
+ * ```
+ *
+ * @param a First number
+ * @param b Second number
+ * @returns The sum of the two numbers
+ */
+export const add = (a: number, b: number) => a + b
