e7309d8bb9c506bb413be37e4ce29ab1ab202cba
stagas <gstagas@gmail.com>
2022-04-27 10:04:30 +0300

    fix: midi types

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -6,7 +6,8 @@
 //   }
 // }
 
-declare class MIDIMessageEvent extends Event {
+declare class MIDIMessageEvent extends Event implements WebMidi.MIDIMessageEvent {
   data: Uint8Array
+  receivedTime: number
   constructor(kind: string, payload?: { data: Uint8Array })
 }
