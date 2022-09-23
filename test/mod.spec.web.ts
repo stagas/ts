31adf371deb2e1cbf66a9a94478ea7d2e2ce6a0c
stagas <gstagas@gmail.com>
2022-09-23 09:06:03 +0300

    feature: dprint arrow prefer parens always

--- test/mod.spec.web.ts
+++
@@ -1,9 +0,0 @@
-import { add } from '../src/mod'
-
-describe('add(a, b)', () => {
-  it('adds two numbers together into a div', () => {
-    const div = document.createElement('div')
-    div.textContent = '' + add(1, 2)
-    expect(div.textContent).toEqual('3')
-  })
-})
