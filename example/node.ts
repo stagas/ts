906eb024b8cf15785f0ac5f797db60ca1303fcdc
stagas <gstagas@gmail.com>
2023-12-10 16:46:05 +0200

    save

--- example/node.ts
+++
@@ -1,3 +0,0 @@
-import { add } from '../src'
-
-console.log(add(1, 2))
--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,9 @@
-/**
- * Adds two numbers together.
- *
- * ```ts
- * add(1, 2) // => 3
- * ```
- *
- * @param a First number
- * @param b Second number
- * @returns The sum of the two numbers
- */
 export const add = (a: number, b: number) => a + b
+
+export function test_add() {
+  describe('add', () => {
+    it('works', () => {
+      expect(add(1, 2)).toEqual(3)
+    })
+  })
+}
